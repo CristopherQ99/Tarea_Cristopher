a28bc0d14dfc278508c494a5ecdc77426fc33f6a
Critopher Quiroz <cristopher.quiroz@correounivalle.edu.co>
2023-12-12 00:36:34 -0500

    Cracion de la segunda Rama

--- Tarea_Cristopher.cpp
+++ Tarea_Cristopher.cpp
@@ -10,8 +10,16 @@ int main() {
     cout << " ##   ##   ##    ##    ##   ##  ##      ##     ##  ## "<< endl;
     cout << "  ####     ##     ##  #####  ####       ##      ####"<< endl;
 
+    //Rama 1
+    cout << endl;
     cout << "CRISTOPHER QUIROZ" << endl;
     cout << "2243522" << endl;
     cout << "Metodologías de desarrollo de software" << endl;
+
+    //Rama 2 
+    cout << endl;
+    cout << "Cristopher Quiroz" << endl;
+    cout << "2243522" << endl;
+    cout << "Que pena con usted profe y gracias, aprendi mucho :)" << endl;
     return 0;
 }
