05c99fe79ee67418ee551f894bd75daa30ced69d
Critopher Quiroz <cristopher.quiroz@correounivalle.edu.co>
2023-12-12 00:25:19 -0500

    Cracion de Rama

--- Tarea_Cristopher.cpp
+++ Tarea_Cristopher.cpp
@@ -10,5 +10,8 @@ int main() {
     cout << " ##   ##   ##    ##    ##   ##  ##      ##     ##  ## "<< endl;
     cout << "  ####     ##     ##  #####  ####       ##      ####"<< endl;
 
+    cout << "CRISTOPHER QUIROZ" << endl;
+    cout << "2243522" << endl;
+    cout << "Metodologías de desarrollo de software" << endl;
     return 0;
 }
